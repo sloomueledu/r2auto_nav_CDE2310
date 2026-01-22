e8de52390e16b6797abb7aeb6b260e59f5ab5d05
sloomueledu <e1525434@u.nus.edu>
2026-01-22 20:51:16 +0800

    updated to fix r2scanner bug

--- r2scanner.py
+++ r2scanner.py
@@ -39,7 +39,7 @@ class Scanner(Node):
         lr2i = np.nanargmin(laser_range)
 
         # log the info
-        self.get_logger().info('Shortest distance at %i degrees' % (1.38 * lr2i))
+        self.get_logger().info('Shortest distance at %i degrees' % (1.4 * lr2i))
 
 
 def main(args=None):
