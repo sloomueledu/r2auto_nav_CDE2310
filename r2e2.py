cbb82b0fd697a711a8e33b1442abe7ad8a7917ab
sloomueledu <e1525434@u.nus.edu>
2026-02-10 17:19:37 +0800

    Updated r2e2 to detect front angles only

--- r2e2.py
+++ r2e2.py
@@ -6,7 +6,9 @@ from std_msgs.msg import String
 import numpy as np
 
 # Define Constants
-DETECT_DIST = 1.0
+stop_dist = 1.0
+front_angle = 30
+front_angle_range = range(-front_angle, front_angle + 1,1)  # -30 to 30 degrees
 
 class LengthDetect(Node):
     def __init__(self):
@@ -27,12 +29,12 @@ class LengthDetect(Node):
     def callback(self, msg):
         
         # Storage for LIDAR data
-        laser_range = np.array(msg.ranges) #create an array to hold LIDAR ranges
+        self.laser_range = np.array(msg.ranges) #create an array to hold LIDAR ranges
 
         # Replace 0.0 (out of range) with NaN 
-        laser_range[laser_range == 0] = np.nan
+        self.laser_range[self.laser_range == 0] = np.nan
         
-        if np.isnan(laser_range).all(): #handles cases whereby all values are OUT OF RANGE (all nan) or EMPTY ARRAY
+        if np.isnan(self.laser_range).all(): #handles cases whereby all values are OUT OF RANGE (all nan) or EMPTY ARRAY
             self.get_logger().info('No LIDAR data received')
             pub_msg = String()
             pub_msg.data = "0"
@@ -40,15 +42,17 @@ class LengthDetect(Node):
             return
         
         else:
-            lr2i = np.nanargmin(laser_range) #finds and saves the index of the closest object
+            lri = (self.laser_range[front_angle_range]<float(stop_dist)).nonzero() #finds the distance of the closest object
+            self.get_logger().info('Distances: %s' % str(lri))
             pub_msg = String() #initialise a message to be sent to RPI
 
-            if laser_range[lr2i] <= DETECT_DIST:    
-                self.get_logger().info('Obstacle detected at distance: %.2f meters' % laser_range[lr2i])
+            if len(lri[0]) > 0: #if there are obstacles in front
+
+                self.get_logger().info('Obstacle detected at distance: %.2f meters at angle %d degrees' % (self.laser_range[lri[0][0]], np.nanargmin(self.laser_range[front_angle_range]) - front_angle)) #log the distance and angle of the closest obstacle in front
                 pub_msg.data = "1"
                 self.publisher_.publish(pub_msg)
             else:
-                self.get_logger().info('No obstacle detected within %.2f meters' % DETECT_DIST)
+                self.get_logger().info('No obstacle detected infront within %.2f meters' % stop_dist)
                 pub_msg.data = "0"
                 self.publisher_.publish(pub_msg)
 
